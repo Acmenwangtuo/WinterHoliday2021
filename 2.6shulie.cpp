f255490d8d4d12a3271eb630178ab93e3dc1578e
Acmenwangtuo <1198653145@qq.com>
2021-02-06 12:03:32 +0800

    bitmeiju

---
+++ 2.6shulie.cpp
@@ -0,0 +1,26 @@
+////
+//// Created by 王拓 on 2021/2/6.
+////
+//#include <iostream>
+//#include <cmath>
+//#include <vector>
+//#include <algorithm>
+//#define ll long long
+//using namespace std;
+//int main(){
+//    int k , n;
+//    cin >> k >> n;
+//    vector<ll> res;
+//    for(int s = 0; s < (1 << 10);s++){
+//        ll cur = 0;
+//        for(int j  = 0;j < 10;j++){
+//            if((1<<j)&s){
+//                cur += (pow(k,j));
+//            }
+//        }
+//        res.push_back(cur);
+//    }
+//    sort(res.begin(),res.end());
+//    cout << res[n] << endl;
+//}
+//
