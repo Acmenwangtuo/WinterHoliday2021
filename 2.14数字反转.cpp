40813b5d1e326e080ff074be00e063bafbe78789
Acmenwangtuo <1198653145@qq.com>
2021-02-14 01:04:05 +0800

    monichafen

---
+++ 2.14数字反转.cpp
@@ -0,0 +1,23 @@
+//
+// Created by 王拓 on 2021/2/14.
+//
+//#include <iostream>
+//#include <cstring>
+//using namespace std;
+//int main(){
+//    int n;
+//    cin >> n;
+//    if(n>0){
+//        string res = to_string(n);
+//        reverse(res.begin(),res.end());
+//        while(res[0]=='0') res.erase(0,1);
+//        cout << res << endl;
+//    }
+//    else{
+//        string res = to_string(n);
+//        reverse(res.begin()+1,res.end());
+//        while(res[1]=='0') res.erase(1,1);
+//        cout << res << endl;
+//    }
+//    return 0;
+//}
